5ee0802809f9a8ca8813ef2051152a851f09b5e5
Timoleroux <timoleroux2006@gmail.com>
2023-03-01 15:37:59 +0100

    Ajout image temporaire + fonctions js

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-function confirmCharactere() {
-    var name = document.getElementById('charactere-name').value;
+function confirmcharacter() {
+    var name = document.getElementById("character-choice").value;
     var gender = document.querySelector('input[name="gender-choice"]:checked').value;
-    document.getElementById('name').innerHTML = name
-    if (name) {
-        return [name, gender]
-    } else {
-        name = "Jean-Dominique" // Générer un nom aléatoire
-        return name, gender
+    if (!name) {
+        if (gender == "male") {
+            var names = ["Jean-Dominique", "Florent", "Sébastien"];
+        } else {
+            var names = ["Marie-Dominique", "Christelle", "Géraldine"];
+        }
+        name = names[Math.floor(Math.random() * names.length)];
+        document.getElementById("character-choice").value = name;
     }
+    document.getElementById("name").innerHTML = name;
+    return [name, gender];
 }
 
-
+script_id = 0;
 function getChoice() {
-    var choice = document.querySelector('input[name="answer-choice"]:checked').value;
+    var choice = document.querySelector(`input[name="answer-choice-${script_id}"]:checked`).value;
     if (choice) {
-        document.getElementById("choice-button").style.display = "none";
-        displayScript(1, choice)
+        //  Permet que les radios button ne se décochent pas après qu'on ait validé la réponse
+        document.querySelector(`input[name="answer-choice-${script_id}"]:checked`).setAttribute("checked", "checked");
+        document.getElementById(`confirm-choice-button-${script_id}`).style.display = "none";
+        script_id = script_id + 1;
+        displayScript(script_id, choice);
     }
 }
 
 function displayScript(script_id, choice) {
-    current_html = document.getElementById('story').innerHTML;
-    new_html = current_html + `
-    <div class="script-container">
-        <p class="script">
+    current_html = document.getElementById("story").innerHTML;
+    new_html =
+        current_html +
+        `
+    <div class="scene-container">
+        <p class="scene">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis id quas perspiciatis quis assumenda nesciunt recusandae laudantium tempore dolorum possimus commodi, sapiente, magni repudiandae quisquam praesentium. Aliquam minima quod expedita!
         </p>
-        <div id="answer">
-            <input type="radio" name="answer-choice" value="answer-1" id="answer-1"> Réponse 1
-            <input type="radio" name="answer-choice" value="answer-2" id="answer-2"> Réponse 2
+        <div>
+            <input type="radio" name="answer-choice-${script_id}" value="answer-1"> Réponse 1
+            <input type="radio" name="answer-choice-${script_id}" value="answer-2"> Réponse 2
         </div>
-        <button id="choice-button" onclick="getChoice()">Valider</button>
+        <button id="confirm-choice-button-${script_id}" onclick="getChoice()">Valider</button>
     </div>
     `;
-    document.getElementById('story').innerHTML = new_html;
+    document.getElementById("story").innerHTML = new_html;
 }
